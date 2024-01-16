1c91b2c6b804e9607a364938610f6995fde5e4cf
RobotOverlord550 <erniewhitmarsh3@gmail.com>
2024-01-16 08:16:26 -0500

    Initial commit

---
+++ src/main/java/robotoverlord/wwiimod/WWIIModClient.java
@@ -0,0 +1,10 @@
+package robotoverlord.wwiimod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WWIIModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/robotoverlord/wwiimod/WWIIModDataGenerator.java
@@ -0,0 +1,11 @@
+package robotoverlord.wwiimod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WWIIModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
